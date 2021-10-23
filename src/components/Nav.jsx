1056e1c678a561ba794d17b0d06dd811dd9a82de
Carlos Gumucio <cgumuciolabbe@gmail.com>
2021-10-22 23:07:50 -0300

    added bootstrap and started with the nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Logo</li>
+        <li>Quests</li>
+        <li>Skill Tree</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
