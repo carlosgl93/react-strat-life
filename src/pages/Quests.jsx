92a624890f61dd81dd41d9072aa80bf4e4e4b1fa
Carlos Gumucio <cgumuciolabbe@gmail.com>
2021-10-26 09:49:53 -0300

    Created the component to create a quest CreateQuestCard and added to the quests view

--- src/pages/Quests.jsx
+++ src/pages/Quests.jsx
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Dropdown from "react-bootstrap/Dropdown";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import reactDom from "react-dom";
-import QuestItem from "../components/quests/QuestItem";
+
 import QuestList from "../components/quests/QuestList";
+import CreateQuestCard from "../components/ui/CreateQuestCard";
 
 const dummy_quests = [
   {
@@ -55,16 +50,6 @@ const dummy_quests = [
 ];
 
 const Quests = () => {
-  const [questTitle, setQuestTitle] = useState("");
-  const [questPriority, setQuestPriority] = useState(1);
-  const [questRewards, setQuestRewards] = useState([]);
-
-  function handleQuestTitle(e) {
-    let questTitle = reactDom.findDOMNode("questTitle");
-    console.log(questTitle);
-    setQuestTitle(questTitle);
-  }
-
   return (
     <Container fluid>
       <div className="top text-center">
@@ -74,58 +59,7 @@ const Quests = () => {
           points
         </p>
       </div>
-      <div className="create">
-        <Card>
-          <Card.Header as="h5">
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="Quest Title"
-                aria-label="qTitle"
-                aria-describedby="qTitle"
-                ref={setQuestTitle}
-              />
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  Priority
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.ItemText>
-                    1 = Do now / 4 = Can be avoided
-                  </Dropdown.ItemText>
-                  <Dropdown.Item>1</Dropdown.Item>
-                  <Dropdown.Item>2</Dropdown.Item>
-                  <Dropdown.Item>3</Dropdown.Item>
-                  <Dropdown.Item>4</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </InputGroup>
-          </Card.Header>
-          <Card.Body>
-            <Card.Text>
-              Add rewards:
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  Pillars affected
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item>Emotional</Dropdown.Item>
-                  <Dropdown.Item>Financial</Dropdown.Item>
-                  <Dropdown.Item>Social</Dropdown.Item>
-                  <Dropdown.Item>Spiritual</Dropdown.Item>
-                  <Dropdown.Item>Occupational</Dropdown.Item>
-                  <Dropdown.Item>Physical</Dropdown.Item>
-                  <Dropdown.Item>Intellectual</Dropdown.Item>
-                  <Dropdown.Item>Environmental</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              <ul></ul>
-            </Card.Text>
-            <Button variant="primary" onClick={handleQuestTitle}>
-              Create Quest
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
+      <CreateQuestCard />
       <div className="quests">
         <QuestList quests={dummy_quests} />
       </div>
