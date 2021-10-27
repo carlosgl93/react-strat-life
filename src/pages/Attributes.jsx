b1a67a5a33641f7fdaee17d53f26372a459e73ea
Carlos Gumucio <cgumuciolabbe@gmail.com>
2021-10-27 12:03:31 -0300

    Progressed in the attributes view. Inserted the explanatory image

--- src/pages/Attributes.jsx
+++ src/pages/Attributes.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+import attributesImage from "../assets/attributes.png";
 
 const Attributes = () => {
-  return <div>Attributes</div>;
+  return (
+    <Container>
+      <h2 className="text-center">Attributes</h2>
+      <p>
+        Life is composed of attributes, our actions ripe and mature into fruits
+        that give us traits . These traits are the attributes that constitute
+        ourselves.
+      </p>
+      <Image src={attributesImage} rounded style={{ width: "80vw" }} />
+    </Container>
+  );
 };
 
 export default Attributes;
