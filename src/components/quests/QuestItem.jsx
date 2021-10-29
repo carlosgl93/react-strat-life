768c9a28c6d6b9c55813e5e095d695b5df7da514
Carlos Gumucio <cgumuciolabbe@gmail.com>
2021-10-29 12:31:52 -0300

    added fetch functionality to get all the quests created

--- src/components/quests/QuestItem.jsx
+++ src/components/quests/QuestItem.jsx
@@ -6,6 +6,11 @@ const QuestItem = (props) => {
       <React.Fragment key={props.quest.id}>
         <li key={props.quest.title}>
           {props.quest.title} priority:{props.quest.priority}
+          <div className="quest_rewards">
+            {props.quest.rewards.map((reward) => {
+              <p>{reward}</p>;
+            })}
+          </div>
         </li>
         <p>Todo: add rewards</p>
       </React.Fragment>
--- src/pages/Quests.jsx
+++ src/pages/Quests.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
@@ -6,9 +6,22 @@ import QuestList from "../components/quests/QuestList";
 import CreateQuestCard from "../components/ui/CreateQuestCard";
 
 const Quests = () => {
+  const [fetching, setFetching] = useState(true);
+  const [loadedQuests, setLoadedQuests] = useState([]);
+
   const history = useHistory();
 
-  let quests = [];
+  const fetchAllUserQuests = () => {
+    fetch("https://strat-life-default-rtdb.firebaseio.com/quests.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setLoadedQuests(data);
+        setFetching(false);
+        console.log(loadedQuests);
+      });
+  };
 
   const handleNewQuest = (questData) => {
     console.log(questData);
@@ -24,24 +37,41 @@ const Quests = () => {
     });
   };
 
-  return (
-    <Container fluid>
-      <div className="top text-center">
-        <h2>Quest Tracker</h2>
-        <p className="description">By doing quests you will earn attributes.</p>
-      </div>
-      <CreateQuestCard onNewQuest={handleNewQuest} />
-      <div className="quests">
-        {quests.length > 0 ? (
-          <QuestList quests={quests} />
-        ) : (
-          <Container className="mt-4 center-text">
-            <strong>No quests yet! Come on, lets have a meaningful day</strong>
-          </Container>
-        )}
-      </div>
-    </Container>
-  );
+  useEffect(() => {
+    setFetching(true);
+    fetchAllUserQuests();
+  }, []);
+
+  if (fetching === true) {
+    return (
+      <Container fluid className="center-text">
+        <h6>Loading your quests</h6>
+      </Container>
+    );
+  } else {
+    return (
+      <Container fluid>
+        <div className="top text-center">
+          <h2>Quest Tracker</h2>
+          <p className="description">
+            By doing quests you will earn attributes.
+          </p>
+        </div>
+        <CreateQuestCard onNewQuest={handleNewQuest} />
+        <div className="quests">
+          {loadedQuests.length > 0 ? (
+            <QuestList quests={loadedQuests} />
+          ) : (
+            <Container className="mt-4 center-text">
+              <strong>
+                No quests yet! Come on, lets have a meaningful day
+              </strong>
+            </Container>
+          )}
+        </div>
+      </Container>
+    );
+  }
 };
 
 export default Quests;
