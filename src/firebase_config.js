a13839efe15156cce249a18168b687618320324f
Carlos Gumucio <cgumuciolabbe@gmail.com>
2021-11-03 08:45:49 -0300

    Added firebase config and setup

---
+++ src/firebase_config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCF3kBnL3QyZyMw-3p_gegwGUoe65I4fto",
+  authDomain: "strat-life.firebaseapp.com",
+  databaseURL: "https://strat-life-default-rtdb.firebaseio.com",
+  projectId: "strat-life",
+  storageBucket: "strat-life.appspot.com",
+  messagingSenderId: "822010862364",
+  appId: "1:822010862364:web:65ace8c1ba01c5f09a38d9",
+  measurementId: "G-3QLWVZG6M0",
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 // Importing the Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FirebaseAppProvider } from "reactfire";
+import firebaseConfig from "./firebase_config";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Suspense fallback={"Loading"}>
+      <App />
+    </Suspense>
+  </FirebaseAppProvider>,
+  document.getElementById("root")
+);
